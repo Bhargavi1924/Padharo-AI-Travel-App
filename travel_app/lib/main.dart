b3cb4f73635ba1e7dd732ccc97ec2c87a5e19371
AvaniDange <dangeavani@gmail.com>
2024-09-06 15:55:58 +0530

    signup

--- travel_app/lib/main.dart
+++ travel_app/lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ItineraryScreen(),
+      home: LoginPage(),
       routes: {
         '/home': (context) => TravelPage(),
       },
